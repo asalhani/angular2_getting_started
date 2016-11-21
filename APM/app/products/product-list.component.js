8f261e969ce7189f39011feed33c52ea27fa54b4
adib salhani <asalhani@outlook.com>
2016-11-21 14:12:28 +0300

    use releating path

--- APM/app/products/product-list.component.js
+++ APM/app/products/product-list.component.js
@@ -47,9 +47,10 @@ var ProductListComponent = (function () {
     };
     ProductListComponent = __decorate([
         core_1.Component({
+            moduleId: module.id,
             selector: 'pm-products',
-            templateUrl: 'app/products/product-list.component.html',
-            styleUrls: ['app/products/product-list.component.css']
+            templateUrl: 'product-list.component.html',
+            styleUrls: ['product-list.component.css']
         }), 
         __metadata('design:paramtypes', [])
     ], ProductListComponent);
