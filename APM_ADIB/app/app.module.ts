88b131bf7973654eb97422baef5ee9f26cc0d0af
adib salhani <asalhani@outlook.com>
2016-11-21 21:53:27 +0300

    apply tutorial on home

--- APM_ADIB/app/app.module.ts
+++ APM_ADIB/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {ProductListComponent} from './products/product-list.component'
+import {ProductFilterPipe} from './products/product-filter.pipe'
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule, FormsModule ],
+  declarations: [ 
+                  AppComponent, 
+                  ProductListComponent,
+                  ProductFilterPipe 
+                ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ APM_ADIB/app/products/product-list.component.ts
@@ -0,0 +1,41 @@
+import {Component} from '@angular/core'
+import {IProduct} from './product'
+
+@Component({
+    moduleId: module.id,
+    selector: "pm-products",
+    templateUrl:"product-list.component.html",
+})
+export class ProductListComponent{
+    pageTitle:string = "List of Products";
+    listFilter:string = "cart";
+    showImage:boolean = false;
+    imageHeigh :number = 30;
+    imageWidth :number = 30;
+    imageMargin: number = 2;
+
+    toggleImage():void{
+        this.showImage = !this.showImage;
+    }
+    products: IProduct[] = [
+    {
+        "productId": 1,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2016",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+        "productId": 2,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2016",
+        "description": "15 gallon capacity rolling garden cart",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+    }];  
+}
