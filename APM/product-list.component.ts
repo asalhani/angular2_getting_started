f236e58a7dfbfddbb6f932f785e392ead6c3bdb1
asalhani <asalhani@outlook.com>
2016-11-22 13:00:12 +0300

    Using Services

---
+++ APM/product-list.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core'
+import {IProduct} from './product'
+import {ProductService} from './product.service'
+@Component({
+    moduleId: module.id,
+    selector:'pm-products',
+    templateUrl: 'product-list.component.html',
+    styleUrls:['product-list.component.css']
+})
+export class ProductListComponent implements OnInit{
+
+    pageTitle:string = 'Product List';
+    imageWidth:number = 60;
+    imageHeight:number = 60;
+    imageMargin:number = 2;
+    showImage:boolean = false;
+    listFilter:string;
+    products:IProduct[];
+
+    constructor(private _productService:ProductService){
+        
+    }
+
+    toggleImages(): void{
+        this.showImage = !this.showImage;
+    }
+
+     ngOnInit(){
+        this.products = this._productService.getProducts();
+    }
+
+    onRatingClicked(message:string):void{
+        this.pageTitle = "Product List: " + message;
+    }
+}
