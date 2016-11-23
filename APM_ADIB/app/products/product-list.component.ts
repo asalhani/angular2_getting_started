c7d26f07ad396de0e959372d3db0151e6bbdb750
adib salhani <asalhani@outlook.com>
2016-11-23 22:08:27 +0300

    format doc

--- APM_ADIB/app/products/product-list.component.ts
+++ APM_ADIB/app/products/product-list.component.ts
@@ -1,35 +1,35 @@
-import {Component, OnInit} from '@angular/core'
-import {IProduct} from './product'
-import {ProductService} from './product.service'
+import { Component, OnInit } from '@angular/core'
+import { IProduct } from './product'
+import { ProductService } from './product.service'
 
 @Component({
     moduleId: module.id,
     selector: "pm-products",
-    templateUrl:"product-list.component.html",
+    templateUrl: "product-list.component.html",
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
 
-    constructor(private _productService : ProductService){
+    constructor(private _productService: ProductService) {
 
     }
 
-    pageTitle:string = "List of Products";
-    listFilter:string = "";
-    showImage:boolean = true;
-    imageHeigh :number = 30;
-    imageWidth :number = 30;
+    pageTitle: string = "List of Products";
+    listFilter: string = "";
+    showImage: boolean = true;
+    imageHeigh: number = 30;
+    imageWidth: number = 30;
     imageMargin: number = 2;
-    errorMessage:string;
-    products:  IProduct[];
+    errorMessage: string;
+    products: IProduct[];
 
-    toggleImage():void{
+    toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
-    ngOnInit():void{
-        this._productService.getProducts() 
-                              .subscribe(products => this.products = products,
-                                  error => this.errorMessage = <any>error);
+    ngOnInit(): void {
+        this._productService.getProducts()
+            .subscribe(products => this.products = products,
+            error => this.errorMessage = <any>error);
     }
 
 
